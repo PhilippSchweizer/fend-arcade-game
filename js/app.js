78757cfbbc2df4c1e76a90bb2c5cfcb763a155e8
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-08-07 16:51:44 +0200

    replace enemy function from starter code with game element sub class

--- js/app.js
+++ js/app.js
@@ -13,22 +13,13 @@ class gameElement {
 }
 
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+const speeds = [210, 305, 400];
+class Enemy extends gameElement {
+	constructor(x, y, sprite = 'images/enemy-bug.png') {
+		super(x, y, sprite);
+		this.speed = speeds[Math.floor(Math.random() * speeds.length)];
+	}
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
